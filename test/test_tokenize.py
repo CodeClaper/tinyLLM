f6126d4ba1dbc69809ae556c2ac44eeec5d46fd0
zhouchao03 <jerryzhou.zc@gmail.com>
2025-11-27 23:17:02 +0800

    test: make up the test.

--- test/test_tokenize.py
+++ test/test_tokenize.py
@@ -6,10 +6,10 @@ from src.tokenize import tokenize_file
 ## test for function <tokenize_text>
 def test_token_text():
     ret = tokenize_text("Hello world, this is a test.")
-    assert ret == ['Hello', 'world', ',', 'this', 'is', 'a', 'test']
+    assert ret == ['Hello', 'world', ',', 'this', 'is', 'a', 'test', "."]
 
 ## test for function <tokenize_file>
 def test_token_file():
     ret = tokenize_file("./the-verdict.txt")
-    assert ret[:20] == [] 
+    assert ret[:20] == ['I', 'HAD', 'always', 'thought', 'Jack', 'Gisburn', 'rather', 'a', 'cheap', 'genius', '--', 'though', 'a', 'good', 'fellow', 'enough', '--', 'so', 'it', 'was'] 
 
