9ccf81593257c13fdc153e029b931c96597599e6
zhouchao03 <jerryzhou.zc@gmail.com>
2025-12-01 17:37:07 +0800

    test: add some test.

--- test/test_my_tiktoken.py
+++ test/test_my_tiktoken.py
@@ -9,3 +9,9 @@ def test_encode():
     text = "Hello, do you like tea? <|endoftext|> In the sunlit terraces of someunknownPlace."
     ids = instance.encode(text)
     assert ids == [15496, 11, 466, 345, 588, 8887, 30, 220, 50256, 554, 262, 4252, 18250, 8812, 2114, 286, 617, 34680, 27271, 13]
+
+def test_encode_file():
+    with open("./the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    ids = instance.encode(raw_text)
+    assert len(ids) == 5146
