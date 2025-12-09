8fc25b9ebc95e8904a795bb8807396372413eca3
zhouchao03 <jerryzhou.zc@gmail.com>
2025-12-09 17:45:12 +0800

    build: change the run_demo.sh

--- run_demo.sh
+++ run_demo.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 python3 -m demo.demo_causel_attention
+echo "=========================================="
 python3 -m demo.demo_embedding
+echo "=========================================="
 python3 -m demo.demo_self_attention
+echo "=========================================="
+python3 -m demo.demo_multi_head_attention
