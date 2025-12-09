48f8002eced0ede3c4128dee37b647b7657948f4
zhouchao03 <jerryzhou.zc@gmail.com>
2025-12-09 16:39:02 +0800

    feat: add demo for CausalAttention

--- demo/demo_causel_attention.py
+++ demo/demo_causel_attention.py
@@ -56,3 +56,4 @@ context_length = batch.shape[1]
 ca = CausalAttention(d_in, d_out, context_length, 0.0)
 context_vecs = ca(batch)
 print("context_vecs.shap:", context_vecs.shape)
+print(context_vecs)
