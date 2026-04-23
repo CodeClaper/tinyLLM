e6777b66c336c7f780c6584eb9c124fc90961257
zhouchao03 <jerryzhou.zc@gmail.com>
2026-04-23 22:39:42 +0800

    fix: fix device choice.

--- demo/demo_train_model_simple.py
+++ demo/demo_train_model_simple.py
@@ -22,7 +22,7 @@ model = GPTModel(GPT_CONFIG_124M)
 total_params = sum(p.numel() for p in model.parameters())
 print(f"Total number of parameters: {total_params}")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
 with open("./test/the-verdict.txt", "r", encoding="utf-8") as file:
     text_data = file.read()
